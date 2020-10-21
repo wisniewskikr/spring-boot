3d2c6d532b7dd32a95c3494760553272a38dd9ac
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2020-10-21 07:56:12 +0200

    Init project rest-api-security-basic

---
+++ rest-api-security-basic/src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	@Value(value = "${api.username}")
+    private String apiUsername;
+    @Value(value = "${api.password}")
+    private String apiPassword;
+	
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+   	
+    	http.authorizeRequests()
+    		.antMatchers("/").hasRole("USER")
+        .and()
+        	.httpBasic()
+        .and()
+        	.csrf().disable();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	
+        auth.inMemoryAuthentication()
+                .withUser(apiUsername).password("{noop}"+apiPassword).roles("USER");
+        
+    }
+    
+
+}
