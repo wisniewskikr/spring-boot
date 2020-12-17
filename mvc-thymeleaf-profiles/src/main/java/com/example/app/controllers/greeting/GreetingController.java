c75f31a8731f1d911d30b10514657c84ff368a7c
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2020-12-17 02:06:08 +0100

    Init project mvc-thymeleaf-profiles

---
+++ mvc-thymeleaf-profiles/src/main/java/com/example/app/controllers/greeting/GreetingController.java
@@ -0,0 +1,22 @@
+package com.example.app.controllers.greeting;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.app.commands.greeting.GreetingCommand;
+
+
+@Controller
+public class GreetingController {
+
+	@RequestMapping(value="/greeting", method = RequestMethod.GET)
+	public String greeting(@ModelAttribute("command") GreetingCommand command) {
+		
+		command.setText("Hello World!");
+		return "home/home";
+		
+	}
+	
+}
