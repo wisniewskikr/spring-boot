2ed963ae4dd184af7e726790c07188098d1218fd
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2020-12-29 10:23:01 +0100

    Init project soap-client-secured

---
+++ soap-client-secured/src/main/java/com/example/configs/WsConfig.java
@@ -0,0 +1,52 @@
+package com.example.configs;
+
+import org.apache.wss4j.dom.WSConstants;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+
+import com.example.we.clients.GreetingWsClient;
+import com.example.we.elements.ObjectFactory;
+
+@Configuration
+public class WsConfig {
+	
+	@Value(value = "${soap.server.uri}")
+	private String serverUri;
+	
+	@Bean
+    public Jaxb2Marshaller marshaller() {
+        
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setPackagesToScan(ObjectFactory.class.getPackage().getName());
+        return marshaller;
+        
+    }
+	
+	@Bean
+	public  GreetingWsClient greetingWsClient(Jaxb2Marshaller marshaller) {
+		
+		GreetingWsClient client = new GreetingWsClient();
+		client.setDefaultUri(serverUri);
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        // Authentication
+        client.getWebServiceTemplate().setInterceptors(new ClientInterceptor[]{ securityInterceptor() });
+        return client;
+		
+	}
+	  
+	@Bean
+    public Wss4jSecurityInterceptor securityInterceptor(){
+        Wss4jSecurityInterceptor wss4jSecurityInterceptor = new Wss4jSecurityInterceptor();
+        wss4jSecurityInterceptor.setSecurementActions("Timestamp UsernameToken");
+        wss4jSecurityInterceptor.setSecurementUsername("user");
+        wss4jSecurityInterceptor.setSecurementPassword("password");
+        wss4jSecurityInterceptor.setSecurementPasswordType(WSConstants.PW_TEXT);
+        return wss4jSecurityInterceptor;
+    }
+
+}
