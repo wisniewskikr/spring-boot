b5dc2701a821492c75adf513425a8d076112818a
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2021-05-03 04:01:40 +0200

    Update rest-api-security-basic

--- rest-api-security-basic/src/main/java/com/example/app/config/SecurityConfig.java
+++ rest-api-security-basic/src/main/java/com/example/app/config/SecurityConfig.java
@@ -10,34 +10,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	
+
 	@Value(value = "${api.username}")
-    private String apiUsername;
-    @Value(value = "${api.password}")
-    private String apiPassword;
+	private String apiUsername;
+	@Value(value = "${api.password}")
+	private String apiPassword;
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-   	
-    	http.authorizeRequests()
-    		.antMatchers("/greeting").hasRole("USER")
-        .and()
-        	.httpBasic()
-        .and()
-        	.csrf()
-        		.disable();
+		http.authorizeRequests()
+			.antMatchers("/greeting").hasRole("USER")
+		.and()
+			.httpBasic()
+		.and()
+			.csrf()
+				.disable();
 
-    }
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	
-        auth.inMemoryAuthentication()
-                .withUser(apiUsername).password("{noop}"+apiPassword).roles("USER");
-        
-    }
-    
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.inMemoryAuthentication()
+			.withUser(apiUsername).password("{noop}"+apiPassword).roles("USER");
+
+	}
 
 }
